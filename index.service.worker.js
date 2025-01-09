1d36261248eafb16454fc58b2e9ccf1075f41940
Achilleas Ioannis Yfantis <achilleasein@gmail.com>
2025-01-09 19:58:14 +0200

    Bullets!!!!

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736444474|21952097';
+const CACHE_VERSION = '1736445342|889680371';
 /** @type {string} */
 const CACHE_PREFIX = 'Mobile supreme-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
