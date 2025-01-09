f01bf76c1bcb91a9e43fa5f0f44cda81bbe25f5d
Achilleas Ioannis Yfantis <achilleasein@gmail.com>
2025-01-09 19:48:57 +0200

    latest version

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736025226|563648403';
+const CACHE_VERSION = '1736444474|21952097';
 /** @type {string} */
 const CACHE_PREFIX = 'Mobile supreme-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
